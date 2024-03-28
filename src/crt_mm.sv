ced358fe1d5e27e2712fff2a8c7b54ef1e938566
dtsunami <dan.tsunami@gmail.com>
2024-03-27 19:01:11 -0700

    Update crt_mm.sv

--- src/crt_mm.sv
+++ src/crt_mm.sv
@@ -1,11 +1,14 @@
 /*
  * Copyright (c) 2024 dfstar
  * SPDX-License-Identifier: Apache-2.0
+ *
+ * Chinese remainder theorem matrix multiplier using quarter squares LUT
+ *
  */
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_dang_crt_mm (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
